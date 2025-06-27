39b0246a1df564f0141b1ee657fc0aab50947312
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-27 10:55:11 +0300

    put all method

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -23,8 +23,12 @@ class TestScreen extends StatelessWidget {
                   // the put method is a future but can be used without await
                   // it add or update data in the box so in key not added it will create a new key
                   // if the key is already present it will update the value
-                  box.put("name", "Abdelhamid");
-                  box.put("age", 35);
+                  /* box.put("name", "Abdelhamid");
+                  box.put("age", 35); */
+                  box.putAll({
+                    "name": "Abdelhamid Ahmed",
+                    "age": 30,
+                  });
                 },
                 child: Text('Put Data')),
             ElevatedButton(
