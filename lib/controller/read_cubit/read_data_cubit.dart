f53787da96b7403f40f967c8c55b82131606521f
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-28 15:32:55 +0300

    opeining box in read cubit

--- lib/controller/read_cubit/read_data_cubit.dart
+++ lib/controller/read_cubit/read_data_cubit.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:learn_flutter_hive/model/word_model.dart';
+import 'package:learn_flutter_hive/utils/app_constants.dart';
 
 part 'read_data_states.dart';
 
 class ReadDataCubit extends Cubit<ReadDataInitialState> {
   static get(context) => BlocProvider.of(context);
   ReadDataCubit() : super(ReadDataInitialState());
+
+  final Box _box = Hive.box(AppConstants.boxName);
 }
