1d8680233fab6f223216ed1733390b5fbb8ba409
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-28 15:43:26 +0300

    adding filter and sort attibutes and methods in read cubit

--- lib/controller/read_cubit/read_data_cubit.dart
+++ lib/controller/read_cubit/read_data_cubit.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:learn_flutter_hive/model/word_model.dart';
 import 'package:learn_flutter_hive/utils/app_constants.dart';
+import 'package:learn_flutter_hive/utils/enums.dart';
 
 part 'read_data_states.dart';
 
@@ -10,4 +11,20 @@ class ReadDataCubit extends Cubit<ReadDataInitialState> {
   ReadDataCubit() : super(ReadDataInitialState());
 
   final Box _box = Hive.box(AppConstants.boxName);
+  LanguageFilter languageFilter = LanguageFilter.allWords;
+  SortBy sortBy = SortBy.time;
+  SortType sortType = SortType.descending;
+
+  void updateLanguageFilter(LanguageFilter filter) {
+    languageFilter = filter;
+  }
+
+  void updateSortBy(SortBy sort) {
+    sortBy = sort;
+  }
+
+  void updateSortType(SortType type) {
+    sortType = type;
+  }
+  
 }
