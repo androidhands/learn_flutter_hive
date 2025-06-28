45980eebf2e15218be54642004d7850dede4e582
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-28 16:35:51 +0300

    I do not trust lecturer way of handling the Hive, he adds more logic that consumes memory and I think that there is another way to add values to box with idex, I will follow another tutorial

--- lib/controller/read_cubit/read_data_cubit.dart
+++ lib/controller/read_cubit/read_data_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:learn_flutter_hive/model/word_model.dart';
@@ -6,7 +8,7 @@ import 'package:learn_flutter_hive/utils/enums.dart';
 
 part 'read_data_states.dart';
 
-class ReadDataCubit extends Cubit<ReadDataInitialState> {
+class ReadDataCubit extends Cubit<ReadDataStates> {
   static get(context) => BlocProvider.of(context);
   ReadDataCubit() : super(ReadDataInitialState());
 
@@ -26,5 +28,16 @@ class ReadDataCubit extends Cubit<ReadDataInitialState> {
   void updateSortType(SortType type) {
     sortType = type;
   }
-  
+
+  void getWords() async {
+    emit(ReadDataLoadingState());
+    try {
+      List<WordModel> wordsToDisplay = [
+        ..._box.get(AppConstants.wordList, defaultValue: <WordModel>[])
+      ].cast<WordModel>();
+    } catch (error) {
+      emit(ReadDataErrorState(
+          'We could not load the words, Please try again later.'));
+    }
+  }
 }
